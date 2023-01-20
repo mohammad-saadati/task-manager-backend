fb75d7d11e7355f0f9f8e5380a28d566e718f778
mohammad_saadati <saadati.m.b@gmail.com>
2023-01-21 03:25:51 +0330

    db connection credentials

--- db/connect.js
+++ db/connect.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const connectDB = async (connectionString) => {
   mongoose.set("strictQuery", false);
 
-  await mongoose.connect(connectionString);
+  await mongoose.connect(connectionString, {
+    authSource: "admin",
+    user: "admin",
+    pass: "password",
+  });
 
   console.log("DB Connected...");
 };
-
 
 module.exports = {
   connectDB,
