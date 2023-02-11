d2c54642ca1263ca8939c4e37a7d8b326fe3320e
mohammad_saadati <saadati.m.b@gmail.com>
2023-02-12 01:34:19 +0330

    register middleware

--- app.js
+++ app.js
@@ -19,25 +19,43 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
+// session
+var cookieSession = require("cookie-session");
+// passport
+const passport = require("passport");
+const passportSetup = require("./utils/googleStrategy");
 
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.set("trust proxy", 1);
 app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberwhole"],
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
   })
 );
-
 app.use(express.static("./public"));
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 // register routes
 const { sendEmail } = require("./controllers/sendEmail");
-app.use("/api/v1/testEmail", sendEmail);
+app.get("/api/v1/send-email", sendEmail);
+app.get("/api/v1/test", (req, res) => {
+  res.status(200).json({ name: "test" });
+});
 app.use("/api/v1", tasks);
 app.use("/api/v1", auth);
 // register middlewares
