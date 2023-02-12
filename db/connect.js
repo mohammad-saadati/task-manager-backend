4c8f19ee4e777e15408a0a47312e4d4bd5471078
mohammad_saadati <saadati.m.b@gmail.com>
2023-02-13 02:14:19 +0330

    set mongo connection to atlassian

--- db/connect.js
+++ db/connect.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 const connectDB = async (connectionString) => {
   mongoose.set("strictQuery", false);
 
-  await mongoose.connect(connectionString, {
-    authSource: "admin",
-    user: "admin",
-    pass: "password",
-  });
+  await mongoose.connect(connectionString);
+  // await mongoose.connect(connectionString, {
+  //   authSource: "admin",
+  //   user: "admin",
+  //   pass: "password",
+  // });
 
   console.log("DB Connected...");
 };
