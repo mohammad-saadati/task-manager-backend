30f6df2b3a0e2fb7e06269cc4cf82a0d94cc1b59
mohammad_saadati <saadati.m.b@gmail.com>
2023-02-14 00:17:33 +0330

    google strategy

--- utils/googleStrategy.js
+++ utils/googleStrategy.js
@@ -7,15 +7,36 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/api/v1/auth/google/calback",
+      callbackURL: "http://localhost:4000/api/v1/auth/google/callback",
       scope: ["profile", "email"],
     },
     function (accessToken, refreshToken, profile, callback) {
+      // User.findOne({ googleId: profile.id }).then((existingUser) => {
+      //   if (existingUser) {
+      //     // we already have a record with the given profile ID
+      //     done(null, existingUser);
+      //   } else {
+      //     // we don't have a user record with this ID, make a new record!
+      //     new User({ googleId: profile.id })
+      //       .save()
+      //       .then((user) => done(null, user));
+      //   }
+      // });
+
       console.log(profile);
       callback(null, profile);
     }
   )
 );
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//   User.findById(id).then((user) => {
+//     done(null, user);
+//   });
+// });
 passport.serializeUser((user, done) => {
   done(null, user);
 });
