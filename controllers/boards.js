da9556c0426385cc8157f1dcad2e3e3c3c064429
mohammad_saadati <saadati.m.b@gmail.com>
2023-02-27 01:17:05 +0330

    board controller initial structure

---
+++ controllers/boards.js
@@ -0,0 +1,46 @@
+// models
+const Board = require("../models/Board");
+// response status
+const { StatusCodes } = require("http-status-codes");
+// errors
+const { NotFoundError } = require("../errors");
+
+const addBoard = async (req, res) => {
+  const board = await Board.create({
+    title: req.title,
+    createdBy: req.user._id,
+  });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ error: false, msg: "Board created", board });
+};
+const getAllBoards = async (req, res) => {
+  const boards = await Board.find({});
+
+  res.status(StatusCodes.OK).json({ error: false, msg: "", boards });
+};
+const getSingleBoard = async (req, res) => {
+  const { id: boardId } = req.params;
+
+  const board = await Board.find({ _id: boardId });
+  // check whether middleware catch the error or not
+  res.status(Status.OK).json({ error: false, msg: "", board });
+};
+const updateSingleBoard = async (req, res) => {
+  const { id: boardId } = req.params;
+
+  const board = await Board.findOneAndUpdate({ _id: boardId }, req.body, {
+    new: true,
+  });
+  // check whether middleware catch the error or not
+  res.status(StatusCodes.OK).json({ err: false, msg: "Board updated", board });
+};
+const deleteSingleBoard = async (req, res) => {};
+
+module.exports = {
+  addBoard,
+  getAllBoards,
+  getSingleBoard,
+  updateSingleBoard,
+  deleteSingleBoard,
+};
