ffe827e71edf7cb42cacf0292ee5078055b6d740
mohammad_saadati <saadati.m.b@gmail.com>
2023-02-27 01:18:01 +0330

    column schema

---
+++ models/Column.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const ColumnSchema = new mongoose.Schema({
+  title: {
+    trim: true,
+    type: String,
+    maxLength: [50, "title must have 50 chars at most."],
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "createdBy is required"],
+  },
+  tasks: [
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task'
+  ],
+  
+}, { timestamps: true });
+
+module.exports = mongoose.model("Column", ColumnSchema);
