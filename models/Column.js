dc51b11a39d9371fabcea2cc80a5a0818ea0a0f1
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-12 19:00:00 +0330

    edits on schema model

--- models/Column.js
+++ models/Column.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 
-const ColumnSchema = new mongoose.Schema({
-  title: {
-    trim: true,
-    type: String,
-    maxLength: [50, "title must have 50 chars at most."],
+const ColumnSchema = new mongoose.Schema(
+  {
+    title: {
+      trim: true,
+      type: String,
+      maxLength: [50, "title must have 50 chars at most."],
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "createdBy is required"],
+    },
+    tasksOrder: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+      },
+    ],
   },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "createdBy is required"],
-  },
-  tasksOrder: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Task'
-  ]},
-  
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Column", ColumnSchema);
