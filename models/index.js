6519bbf852e9fbdd1345f711d9347eb00b2ac700
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-23 01:56:25 +0430

    baron export for models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Board = require("./Board");
+const Column = require("./Column");
+const Task = require("./Task");
+const User = require("./User");
+
+module.exports = {
+  Board,
+  Column,
+  Task,
+  User,
+};
