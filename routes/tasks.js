953dceb2600ea7c5fc5ac6f0b564e716cb65bc61
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-29 13:38:12 +0430

    add route for reorder task

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,14 +7,15 @@ const {
   getSingleTask,
   updateSingleTask,
   deleteSingleTask,
+  reorderTask,
 } = require("../controllers/tasks");
 
 router.route("/tasks").get(getAllTasks).post(addTask);
+router.route("/tasks/reorder").put(reorderTask);
 router
   .route("/tasks/:id")
   .get(getSingleTask)
   .put(updateSingleTask)
   .delete(deleteSingleTask);
-
 
 module.exports = router;
