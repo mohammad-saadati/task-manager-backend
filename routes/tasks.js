ebcac538398c801c8009a8892689d3c710d5ff17
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-03 14:56:08 +0430

    moveTask route handler

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,10 +8,12 @@ const {
   updateSingleTask,
   deleteSingleTask,
   reorderTask,
+  moveTask,
 } = require("../controllers/tasks");
 
 router.route("/tasks").get(getAllTasks).post(addTask);
 router.route("/tasks/reorder").put(reorderTask);
+router.route("/tasks/move").put(moveTask);
 router
   .route("/tasks/:id")
   .get(getSingleTask)
