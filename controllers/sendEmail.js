160ad13b75f6769574962dc6ac6a9193d9ad9573
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-04 23:13:10 +0430

    refactor

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -23,21 +23,7 @@ const sendEmail = async (req, res) => {
 
   res.json(info);
 };
-// const sendEmail = async (req, res) => {
-//   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-//   const msg = {
-//     to: "saadati.m.b@gmail.com", // Change to your recipient
-//     from: "fordevelopmentdesire@gmail.com", // Change to your verified sender
-//     subject: "Sending with SendGrid is Fun",
-//     text: "and easy to do anywhere, even with Node.js",
-//     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-//   };
-
-//   const info = await sgMail.send(msg);
-
-//   res.json(info);
-// };
 
 module.exports = {
   sendEmail,
