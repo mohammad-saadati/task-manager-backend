dcec37b3b08f4a70fd1785bd7e3b34b7ec12139b
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-13 23:55:05 +0430

    edit message

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -30,7 +30,7 @@ const getSingleTask = async (req, res, next) => {
   const task = await Task.findOne({ _id: taskId });
 
   if (!task) {
-    return res.status(404).json({ msg: `there is no task with id: ${taskId}` });
+    return res.status(404).json({ msg: `There is no task with id: ${taskId}` });
   }
 
   res.status(StatusCodes.OK).json({ error: false, msg: "", task });
